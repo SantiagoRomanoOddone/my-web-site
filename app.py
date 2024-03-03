0c25e21bb1fff81857be3f97656c087a74ed34b5
SantiagoRomanoOddone <santiagoromano15@gmail.com>
2024-03-02 23:25:09 -0300

    adding some info

--- app.py
+++ app.py
@@ -86,109 +86,105 @@ with st.container():
 
 
 # CV section
-st.write('<div id="cv"></div>', unsafe_allow_html=True)  # Anchor for CV link
-st.header("CV: Santiago Romano Oddone")
-st.write("Machine Learning Engineer / Data Scientist")
-st.write(
-    """
-    I'm a Machine Learning Engineer with a solid background in industrial engineering, currently pursuing a Master's in Management and Analytics.
-    I specialize in developing AI models to predict scenarios and uncover meaningful patterns in data. With a focus on applying advanced analytics, I am poised to contribute innovative solutions to real-world business challenges.
-    """
-)
-st.write("santiagoromano15@gmail.com +5493884840234")
-st.write("Buenos Aires, Argentina")
-st.write("[Kaggle Profile](www.kaggle.com/santiagoromanooddone)")
-st.write("[LinkedIn Profile](linkedin.com/in/santiagoromanooddone)")
-st.write("[GitHub Profile](github.com/SantiagoRomanoOddone)")
-st.write("[Instagram Profile](instagram.com/santirom15)")
-
-# Work Experience section
-st.write("---")
-st.header("Work Experience")
-
-# Accenture
-st.subheader("Machine Learning Engineer")
-st.write("Accenture Argentina")
-st.write("08/2023 - Present, Buenos Aires, Argentina")
-st.write(
-    """
+with st.container():
+    st.markdown("---")
+    st.header("CV: Santiago Romano Oddone")
+    st.markdown("""
+    ### Machine Learning Engineer / Data Scientist
+    
+    I'm a Machine Learning Engineer with a solid background in industrial engineering, currently pursuing a Master's in Management and Analytics. I specialize in developing AI models to predict scenarios and uncover meaningful patterns in data. With a focus on applying advanced analytics, I am poised to contribute innovative solutions to real-world business challenges.
+    
+    **Contact:** santiagoromano15@gmail.com | +5493884840234
+    **Location:** Buenos Aires, Argentina
+    
+    **Profiles:**
+    - [Kaggle](www.kaggle.com/santiagoromanooddone)
+    - [LinkedIn](linkedin.com/in/santiagoromanooddone)
+    - [GitHub](github.com/SantiagoRomanoOddone)
+    - [Instagram](instagram.com/santirom15)
+    """)
+    
+    # Work Experience section
+    st.markdown("---")
+    st.header("Work Experience")
+    
+    # Accenture
+    st.subheader("Machine Learning Engineer")
+    st.write("Accenture Argentina")
+    st.write("08/2023 - Present, Buenos Aires, Argentina")
+    st.write("""
     - Managing the implementation and optimization of worldwide demand and supply prediction models in production environments.
     - Collaborating effectively with data science teams to analyze monthly results and enhance the predictive capabilities of the models.
-    """
-)
-
-st.subheader("Application Developer")
-st.write("Accenture Argentina")
-st.write("02/2022 - 07/2023, Buenos Aires, Argentina")
-st.write(
-    """
+    """)
+    
+    st.subheader("Application Developer")
+    st.write("Accenture Argentina")
+    st.write("02/2022 - 07/2023, Buenos Aires, Argentina")
+    st.write("""
     - Building supervised and unsupervised Machine Learning models for client clustering and cluster prediction.
     - Developing and managing ETL packages, projects, and solutions for importing/exporting, loading, and processing data.
     - Developing and managing .NET CORE applications.
-    """
-)
-
-# MuniraFoods
-st.subheader("Business Analyst")
-st.write("MuniraFoods")
-st.write("03/2021 - 09/2021, Córdoba, Argentina")
-st.write(
-    """
+    """)
+    
+    # MuniraFoods
+    st.subheader("Business Analyst")
+    st.write("MuniraFoods")
+    st.write("03/2021 - 09/2021, Córdoba, Argentina")
+    st.write("""
     - Monitoring and analyzing operational processes by creating Key Performance Indicators (KPIs) for each product line.
     - Identifying and monitoring operational and compliance risks.
     - Applying continuous improvement techniques to production and logistics processes.
-    """
-)
-
-# Education section
-st.write("---")
-st.header("Education")
-
-st.subheader("Master in Management + Analytics")
-st.write("Torcuato Di Tella University")
-st.write("03/2023 - Present, Buenos Aires, Argentina")
-
-st.subheader("Industrial Engineering")
-st.write("National Technological University")
-st.write("03/2015 - 12/2020, Córdoba, Argentina")
-st.write("GPA: 8.35/10")
-
-# Programming Languages section
-st.write("---")
-st.header("Programming Languages")
-st.write("- Python")
-st.write("- SQL")
-st.write("- R")
-st.write("- C#")
-st.write("- GitHub")
-
-# Certificates section
-st.write("---")
-st.header("Certificates")
-st.write("- Deep Learning Specialization (12/2023), Provider: Deeplearning.ai")
-st.write("- AWS Certified Cloud Practitioner (11/2023), Provider: AWS")
-st.write("- Modern Natural Language Processing in Python (01/2023), Provider: Udemy")
-
-# Professional Skills section
-st.write("---")
-st.header("Professional Skills")
-st.write("- Problem-solving")
-st.write("- Proactive")
-st.write("- Analytical thinking")
-st.write("- Continuous Learning")
-st.write("- Effective communication")
-
-# Languages section
-st.write("---")
-st.header("Languages")
-st.write("- Spanish")
-st.write("- English")
-
-# Interests section
-st.write("---")
-st.header("Interests")
-st.write("- Artificial Intelligence")
-st.write("- Business Analytics")
+    """)
+    
+    # Education section
+    st.markdown("---")
+    st.header("Education")
+    
+    st.subheader("Master in Management + Analytics")
+    st.write("Torcuato Di Tella University")
+    st.write("03/2023 - Present, Buenos Aires, Argentina")
+    
+    st.subheader("Industrial Engineering")
+    st.write("National Technological University")
+    st.write("03/2015 - 12/2020, Córdoba, Argentina")
+    st.write("GPA: 8.35/10")
+    
+    # Programming Languages section
+    st.markdown("---")
+    st.header("Programming Languages")
+    st.write("- Python")
+    st.write("- SQL")
+    st.write("- R")
+    st.write("- C#")
+    st.write("- GitHub")
+    
+    # Certificates section
+    st.markdown("---")
+    st.header("Certificates")
+    st.write("- Deep Learning Specialization (12/2023), Provider: Deeplearning.ai")
+    st.write("- AWS Certified Cloud Practitioner (11/2023), Provider: AWS")
+    st.write("- Modern Natural Language Processing in Python (01/2023), Provider: Udemy")
+    
+    # Professional Skills section
+    st.markdown("---")
+    st.header("Professional Skills")
+    st.write("- Problem-solving")
+    st.write("- Proactive")
+    st.write("- Analytical thinking")
+    st.write("- Continuous Learning")
+    st.write("- Effective communication")
+    
+    # Languages section
+    st.markdown("---")
+    st.header("Languages")
+    st.write("- Spanish")
+    st.write("- English")
+    
+    # Interests section
+    st.markdown("---")
+    st.header("Interests")
+    st.write("- Artificial Intelligence")
+    st.write("- Business Analytics")
 
 
 # # ---- CONTACT ----
